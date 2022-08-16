f5c15764e34e7d3875c224b43e21588a1fd6f827
fbciriaco <fb.ciriaco@gmail.com>
2022-08-16 17:23:35 -0300

    header

--- webpack.config.js
+++ webpack.config.js
@@ -14,12 +14,12 @@ module.exports = {
     resolve: {
         extensions: ['', '.js', '.jsx'],
         alias: {
-            modules: __dirname + 'node_modules',
+            modules: __dirname + '/node_modules',
             jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
             bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js'
         }
     },
-    plugins: [
+    plugins: [ 
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery',
@@ -32,15 +32,15 @@ module.exports = {
             test: /.js[x]?$/,
             loader: 'babel-loader',
             exclude: /node_modules/,
-            query:{ 
-                presets: ['es2015', 'react' ],
+            query: {
+                presets: ['es2015', 'react'],
                 plugins: ['transform-object-rest-spread']
-        }
+            }
         }, {
             test: /\.css$/,
             loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
-        },{
-            test: /\.woff|.woff2|.tff|.eot|.svg|.png|.jpg*.*$/,
+        }, {
+            test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
             loader: 'file'
         }]
     }

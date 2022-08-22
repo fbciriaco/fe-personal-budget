1c3e0b487c101080ef179e8f174e1bf09cfdfb2f
fbciriaco <fb.ciriaco@gmail.com>
2022-08-22 14:22:43 -0300

    Finalização CSS

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -36,13 +36,13 @@ class BillingCycle extends Component {
                 <List />
               </TabContent>
               <TabContent id="tabCreate">
-                <Form onSubmit={this.props.create}/>
+                <Form onSubmit={this.props.create} submitClass="primary"  submitLabel="Incluir" />
               </TabContent>
               <TabContent id="tabUpdate">
-                <Form onSubmit={this.props.update} />
+                <Form onSubmit={this.props.update} submitLabel="Alterar" submitClass="info" />
               </TabContent>
               <TabContent id="tabDelete">
-                <Form onSubmit={this.props.remove} readOnly={true} />
+                <Form onSubmit={this.props.remove} readOnly={true} submitLabel="Excluir" submitClass="danger" />
               </TabContent>
             </TabsContent>
           </Tabs>
